7ac11c2c0b3cd9ff98304f0d07a7757a9d661344
cahindo <erikfaderik@gmail.com>
2021-04-18 22:45:08 +0700

    update D E

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -10,7 +10,7 @@
 #include <time.h>
 #include <wait.h>
 
-void main(){
+void main(int argc, char *argv[]){
   pid_t pid, sid;
   pid = fork();
 
@@ -33,124 +33,169 @@ void main(){
   close(STDERR_FILENO);
 
   char nmFolder[200];
+  chdir("/home/boi");
 
-  while (1){
-    pid_t pid3;
-    pid3 = fork();
-    if (pid3 == 0){
-      pid_t pidA;
+  // ! SUB SOAL E
+  if(argc > 1){
+    if(strcmp(argv[1], "-z") == 0 || strcmp(argv[1], "-x") == 0){
 
-      // ! SUBSOAL A
-      time_t ta = time(NULL);
-      struct tm tms = *localtime(&ta);
-      sprintf(nmFolder, 
-              "/home/boi/%02d-%02d-%d_%02d:%02d:%02d", 
-              tms.tm_mday, 
-              tms.tm_mon + 1, 
-              tms.tm_year + 1900, 
-              tms.tm_hour,
-              tms.tm_min, 
-              tms.tm_sec);
+      // ! SUB SOAL D
+      FILE *killerFile;
+      killerFile = fopen("Killer.sh", "w");
+      
+      char bang[] = "#!/bin/bash";
+      char rmKiller[] = "rm -f Killer.sh";
+      char fullCommand[200];
 
-      pidA = fork();
-      int stsA;
-      chdir("/home/boi");
-
-      if (pidA < 0)
-        exit(EXIT_FAILURE);
-      if (pidA == 0){
-        char *argv[] = {"mkdir", nmFolder, NULL};
-        execv("/bin/mkdir", argv);
-      } else {
-      // ! SUBSOAL B
-        while ((wait(&stsA)) > 0);
-        char curFolder[200];
-        strcpy(curFolder, nmFolder);
-        chdir(curFolder);
-
-        int lop = 0;
-        while (lop < 10) {
-          int epoch = time(NULL);
-          char linkDL[200];
-          sprintf(linkDL, "https://picsum.photos/50");
-
-          char nmFile[200];
-          time_t tb = time(NULL);
-          struct tm tms = *localtime(&tb);
-          sprintf(nmFile, "%d-%02d-%02d_%02d:%02d:%02d.jpg", 
-                  tms.tm_year + 1900, 
-                  tms.tm_mon + 1, 
-                  tms.tm_mday, 
-                  tms.tm_hour,
-                  tms.tm_min, 
-                  tms.tm_sec);
-          
-          pid_t pidB;
-          pidB = fork();
-
-          if (pidB == 0) {
-            char *argv[] = {"wget", "-qO", nmFile, linkDL, NULL};
-            execv("/bin/wget", argv);
-            exit(0);
-          }
-
-          lop++;
-          sleep(5);
-        }
-        // ! SUBSOAL C
-        FILE *stsFile;
-        stsFile = fopen("status.txt", "w");
-
-        if(stsFile == NULL)
-          exit(EXIT_FAILURE);
-
-        char success[50] = "Download Success", temp;
-        // -- ALGORITMA CAESAR CIPER
-        for(int i = 0; success[i] != '\0'; ++i){
-          temp = success[i];
-          
-          if(temp >= 'a' && temp <= 'z'){
-            temp += 5;
-            if(temp > 'z'){
-              temp = temp - 'z' + 'a' - 1;
-            }
-            
-            success[i] = temp;
-          }
-          else if(temp >= 'A' && temp <= 'Z'){
-            temp += 5;
-            if(temp > 'Z'){
-              temp = temp - 'Z' + 'A' - 1;
-            }
-            
-            success[i] = temp;
-          }
-        }
-
-        fputs(success, stsFile);
-        fclose(stsFile);
-
-        chdir("/home/boi");
-        pid_t pidC;
-        pidC = fork();
-        int stsC;
-
-        if(pidC == 0){
-          char nmZip[300];
-          sprintf(nmZip, "%s.zip", nmFolder);
-          char *argv[] = {"zip", "-r", nmZip, nmFolder, NULL};
-          execv("/bin/zip", argv);
-        }else{
-          while(wait(&stsC) > 0);
-
-          char *argv[]={"rm", "-rf", nmFolder, NULL};
-          execv("/bin/rm", argv);
-        }
-
-        exit(EXIT_SUCCESS);
+      char *execArg = &argv[0][2];
+      if (!strcmp(argv[1], "-z")){
+        sprintf(fullCommand, "ps -C %s -o pid= | xargs -r kill -9", execArg);
+      }else if (!strcmp(argv[1], "-x")){
+        sprintf(fullCommand, "ps -C %s -o pid= -o stat= | awk '/Ss/ {print $1}' | xargs -r kill -9", execArg);
       }
+
+      fputs(bang, killerFile);
+      fputs("\n", killerFile);
+      fputs(fullCommand, killerFile);
+      fputs("\n", killerFile);
+      fputs(rmKiller, killerFile);
+      fclose(killerFile);
+
+      // pid_t pidD;
+      // pidD = fork();
+      // if(pidD == 0){
+      //   char *argv[]={"bash", "Killer.sh"};
+      //   execv("/bin/bash", argv);
+      //   exit(0);
+      // }
+
+      char commandBash[50];
+      sprintf(commandBash, "chmod u+x %s && ./%s", "Killer.sh", "Killer.sh");
+      FILE *runBash = popen(commandBash, "w");
+      pclose(runBash);
+
     }
-    sleep(40);
+  } 
+  else {
+    while (1){
+      pid_t pid3;
+      pid3 = fork();
+      if (pid3 == 0){
+        pid_t pidA;
+
+        // ! SUBSOAL A
+        time_t ta = time(NULL);
+        struct tm tms = *localtime(&ta);
+        sprintf(nmFolder, 
+                "/home/boi/%02d-%02d-%d_%02d:%02d:%02d", 
+                tms.tm_mday, 
+                tms.tm_mon + 1, 
+                tms.tm_year + 1900, 
+                tms.tm_hour,
+                tms.tm_min, 
+                tms.tm_sec);
+
+        pidA = fork();
+        int stsA;
+        chdir("/home/boi");
+
+        if (pidA < 0)
+          exit(EXIT_FAILURE);
+        if (pidA == 0){
+          char *argv[] = {"mkdir", nmFolder, NULL};
+          execv("/bin/mkdir", argv);
+        } else {
+        // ! SUBSOAL B
+          while ((wait(&stsA)) > 0);
+          char curFolder[200];
+          strcpy(curFolder, nmFolder);
+          chdir(curFolder);
+
+          int lop = 0;
+          while (lop < 10) {
+            int epoch = time(NULL);
+            char linkDL[200];
+            sprintf(linkDL, "https://picsum.photos/50");
+
+            char nmFile[200];
+            time_t tb = time(NULL);
+            struct tm tms = *localtime(&tb);
+            sprintf(nmFile, "%d-%02d-%02d_%02d:%02d:%02d.jpg", 
+                    tms.tm_year + 1900, 
+                    tms.tm_mon + 1, 
+                    tms.tm_mday, 
+                    tms.tm_hour,
+                    tms.tm_min, 
+                    tms.tm_sec);
+            
+            pid_t pidB;
+            pidB = fork();
+
+            if (pidB == 0) {
+              char *argv[] = {"wget", "-qO", nmFile, linkDL, NULL};
+              execv("/bin/wget", argv);
+              exit(0);
+            }
+
+            lop++;
+            sleep(5);
+          }
+          // ! SUBSOAL C
+          FILE *stsFile;
+          stsFile = fopen("status.txt", "w");
+
+          if(stsFile == NULL)
+            exit(EXIT_FAILURE);
+
+          char success[50] = "Download Success", temp;
+          // -- ALGORITMA CAESAR CIPER
+          for(int i = 0; success[i] != '\0'; ++i){
+            temp = success[i];
+            
+            if(temp >= 'a' && temp <= 'z'){
+              temp += 5;
+              if(temp > 'z'){
+                temp = temp - 'z' + 'a' - 1;
+              }
+              
+              success[i] = temp;
+            }
+            else if(temp >= 'A' && temp <= 'Z'){
+              temp += 5;
+              if(temp > 'Z'){
+                temp = temp - 'Z' + 'A' - 1;
+              }
+              
+              success[i] = temp;
+            }
+          }
+
+          fputs(success, stsFile);
+          fclose(stsFile);
+
+          chdir("/home/boi");
+          pid_t pidC;
+          pidC = fork();
+          int stsC;
+
+          if(pidC == 0){
+            char nmZip[300];
+            sprintf(nmZip, "%s.zip", nmFolder);
+            char *argv[] = {"zip", "-r", nmZip, nmFolder, NULL};
+            execv("/bin/zip", argv);
+          }else{
+            while(wait(&stsC) > 0);
+
+            char *argv[]={"rm", "-rf", nmFolder, NULL};
+            execv("/bin/rm", argv);
+          }
+
+          exit(EXIT_SUCCESS);
+          // Selesai SUB B dan C
+        }
+      }
+      sleep(40);
+    }
+    exit(0);
   }
-  exit(0);
 }
