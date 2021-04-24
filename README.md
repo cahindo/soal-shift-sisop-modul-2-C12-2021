bba9ce6c2992a52a0dfb0155a8e8a5921808393f
cahindo <erikfaderik@gmail.com>
2021-04-25 01:22:02 +0700

    fix no 3 and update readme A

--- README.md
+++ README.md
@@ -1,19 +1,77 @@
 # Laporan Penjelasan Soal Shift Modul 2
 
 ## Anggota
+
 - Mohammad Faderik Izzul Haq (05111940000023)
 - Jonathan Timothy Siregar (05111940000120)
 - Abiya Sabitta Ragadani (05111940000166)
 
 ## SOAL 1
+
 ## SOAL 2
+
 ## SOAL 3
-Pada soal ini kita diminta membuat program yang berupa daemon proses.
+
+Pada soal ini kita diminta membuat program yang berupa daemon proses yang akan mendownload file dari link tertentu berdasarkan waktu proses tersebut berjalan.
+
 ### Kendala yang dihadapi
+
 Pada pengerjaan soal ini, kami sempat mengalami kendala karena command wget tidak bisa berjalan dari exec. Kami sempat stuck karena tidak mengetahui error nya terletak dimana. Namun setelah kami coba run program yang sama di komputer lain, ternyata program bisa berjalan. Jadi kami simpulkan komputer pertama yang kami gunakan (Ubuntu 16.04) terdapat masalah yang sampai saat ini belum kami ketahui. Selain itu pada masa pengerjaan soal shift kami belum banyak tahu tentang argumen program dan kill program dari dalam program. Sehingga kami belum sempat selesai mengerjakan sampai ke sub soal D dan E.
+
 ### Sub Soal A
 
+Pada soal A kita diminta membuat direktori setiap 40 detik dengan nama direktori merupakan waktu saat ini dengan format sesuai timestamp [YYYY-mm-dd_HH:ii:ss].
+
+```cpp
+int main(){
+while (1)
+    {
+      // program utama
+    }
+    sleep(40);
+}
+
+```
+
+Program soal A ini secara keseluruhan berada dalam while daemon seperti kode diatas.
+
+```cpp
+  time_t ta = time(NULL);
+  struct tm tms = *localtime(&ta);
+  sprintf(nmFolder,
+          "%02d-%02d-%d_%02d:%02d:%02d",
+          tms.tm_mday,
+          tms.tm_mon + 1,
+          tms.tm_year + 1900,
+          tms.tm_hour,
+          tms.tm_min,
+          tms.tm_sec);
+  pid_t pidA;
+  pidA = fork();
+  int stsA;
+  chdir("/home/boi");
+
+  if (pidA < 0)
+    exit(EXIT_FAILURE);
+  if (pidA == 0)
+  {
+    char *argv[] = {"mkdir", nmFolder, NULL};
+    execv("/bin/mkdir", argv);
+  }
+```
+
+Program diatas merupakan child proses untuk membuat folder dengan nama direktori dari string yang digenerate dari waktu saat ini.
+
 ### Sub Soal B
+
 ### Sub Soal C
+
 ### Sub Soal D
+
+        // Print only the process IDs of syslogd:
+        // ps -C syslogd -o pid
+        // -r, --no-run-if-empty
+        // If the standard input does not  contain  any  non‐
+        // blanks,  do  not  run  the command.
+
 ### Sub Soal E
--- soal3/soal3.c
+++ soal3/soal3.c
@@ -10,7 +10,8 @@
 #include <time.h>
 #include <wait.h>
 
-void main(int argc, char *argv[]){
+void main(int argc, char *argv[])
+{
   pid_t pid, sid;
   pid = fork();
 
@@ -22,7 +23,7 @@ void main(int argc, char *argv[]){
   umask(0);
 
   sid = setsid();
-  if (sid < 0) 
+  if (sid < 0)
     exit(EXIT_FAILURE);
 
   if ((chdir("/")) < 0)
@@ -36,21 +37,26 @@ void main(int argc, char *argv[]){
   chdir("/home/boi");
 
   // ! SUB SOAL E
-  if(argc > 1){
-    if(strcmp(argv[1], "-z") == 0 || strcmp(argv[1], "-x") == 0){
+  if (argc > 1)
+  {
+    if (strcmp(argv[1], "-z") == 0 || strcmp(argv[1], "-x") == 0)
+    {
 
       // ! SUB SOAL D
       FILE *killerFile;
       killerFile = fopen("Killer.sh", "w");
-      
+
       char bang[] = "#!/bin/bash";
       char rmKiller[] = "rm -f Killer.sh";
       char fullCommand[200];
 
       char *execArg = &argv[0][2];
-      if (!strcmp(argv[1], "-z")){
+      if (!strcmp(argv[1], "-z"))
+      {
         sprintf(fullCommand, "ps -C %s -o pid= | xargs -r kill -9", execArg);
-      }else if (!strcmp(argv[1], "-x")){
+      }
+      else if (!strcmp(argv[1], "-x"))
+      {
         sprintf(fullCommand, "ps -C %s -o pid= -o stat= | awk '/Ss/ {print $1}' | xargs -r kill -9", execArg);
       }
 
@@ -61,77 +67,77 @@ void main(int argc, char *argv[]){
       fputs(rmKiller, killerFile);
       fclose(killerFile);
 
-      // pid_t pidD;
-      // pidD = fork();
-      // if(pidD == 0){
-      //   char *argv[]={"bash", "Killer.sh"};
-      //   execv("/bin/bash", argv);
-      //   exit(0);
-      // }
-
       char commandBash[50];
-      sprintf(commandBash, "chmod u+x %s && ./%s", "Killer.sh", "Killer.sh");
+      sprintf(commandBash, "chmod u+x %s", "Killer.sh");
       FILE *runBash = popen(commandBash, "w");
       pclose(runBash);
-
     }
-  } 
-  else {
-    while (1){
+  }
+  else
+  {
+    while (1)
+    {
       pid_t pid3;
       pid3 = fork();
-      if (pid3 == 0){
-        pid_t pidA;
-
+      if (pid3 == 0)
+      {
         // ! SUBSOAL A
         time_t ta = time(NULL);
         struct tm tms = *localtime(&ta);
-        sprintf(nmFolder, 
-                "/home/boi/%02d-%02d-%d_%02d:%02d:%02d", 
-                tms.tm_mday, 
-                tms.tm_mon + 1, 
-                tms.tm_year + 1900, 
+        sprintf(nmFolder,
+                "%02d-%02d-%d_%02d:%02d:%02d",
+                tms.tm_mday,
+                tms.tm_mon + 1,
+                tms.tm_year + 1900,
                 tms.tm_hour,
-                tms.tm_min, 
+                tms.tm_min,
                 tms.tm_sec);
 
+        pid_t pidA;
         pidA = fork();
         int stsA;
         chdir("/home/boi");
 
         if (pidA < 0)
           exit(EXIT_FAILURE);
-        if (pidA == 0){
+        if (pidA == 0)
+        {
           char *argv[] = {"mkdir", nmFolder, NULL};
           execv("/bin/mkdir", argv);
-        } else {
-        // ! SUBSOAL B
-          while ((wait(&stsA)) > 0);
+        }
+        else
+        {
+          // ! SUBSOAL B
+          while ((wait(&stsA)) > 0)
+            ;
           char curFolder[200];
           strcpy(curFolder, nmFolder);
           chdir(curFolder);
 
           int lop = 0;
-          while (lop < 10) {
+          while (lop < 10)
+          {
             int epoch = time(NULL);
             char linkDL[200];
-            sprintf(linkDL, "https://picsum.photos/50");
+            // sprintf(linkDL, "https://picsum.photos/50");
+            sprintf(linkDL, "%s/%d", "https://picsum.photos", (epoch % 1000) + 5);
 
             char nmFile[200];
             time_t tb = time(NULL);
             struct tm tms = *localtime(&tb);
-            sprintf(nmFile, "%d-%02d-%02d_%02d:%02d:%02d.jpg", 
-                    tms.tm_year + 1900, 
-                    tms.tm_mon + 1, 
-                    tms.tm_mday, 
+            sprintf(nmFile, "%d-%02d-%02d_%02d:%02d:%02d.jpg",
+                    tms.tm_year + 1900,
+                    tms.tm_mon + 1,
+                    tms.tm_mday,
                     tms.tm_hour,
-                    tms.tm_min, 
+                    tms.tm_min,
                     tms.tm_sec);
-            
+
             pid_t pidB;
             pidB = fork();
 
-            if (pidB == 0) {
+            if (pidB == 0)
+            {
               char *argv[] = {"wget", "-qO", nmFile, linkDL, NULL};
               execv("/bin/wget", argv);
               exit(0);
@@ -144,28 +150,33 @@ void main(int argc, char *argv[]){
           FILE *stsFile;
           stsFile = fopen("status.txt", "w");
 
-          if(stsFile == NULL)
+          if (stsFile == NULL)
             exit(EXIT_FAILURE);
 
           char success[50] = "Download Success", temp;
           // -- ALGORITMA CAESAR CIPER
-          for(int i = 0; success[i] != '\0'; ++i){
+          for (int i = 0; success[i] != '\0'; ++i)
+          {
             temp = success[i];
-            
-            if(temp >= 'a' && temp <= 'z'){
+
+            if (temp >= 'a' && temp <= 'z')
+            {
               temp += 5;
-              if(temp > 'z'){
+              if (temp > 'z')
+              {
                 temp = temp - 'z' + 'a' - 1;
               }
-              
+
               success[i] = temp;
             }
-            else if(temp >= 'A' && temp <= 'Z'){
+            else if (temp >= 'A' && temp <= 'Z')
+            {
               temp += 5;
-              if(temp > 'Z'){
+              if (temp > 'Z')
+              {
                 temp = temp - 'Z' + 'A' - 1;
               }
-              
+
               success[i] = temp;
             }
           }
@@ -178,15 +189,19 @@ void main(int argc, char *argv[]){
           pidC = fork();
           int stsC;
 
-          if(pidC == 0){
+          if (pidC == 0)
+          {
             char nmZip[300];
             sprintf(nmZip, "%s.zip", nmFolder);
             char *argv[] = {"zip", "-r", nmZip, nmFolder, NULL};
             execv("/bin/zip", argv);
-          }else{
-            while(wait(&stsC) > 0);
+          }
+          else
+          {
+            while (wait(&stsC) > 0)
+              ;
 
-            char *argv[]={"rm", "-rf", nmFolder, NULL};
+            char *argv[] = {"rm", "-rf", nmFolder, NULL};
             execv("/bin/rm", argv);
           }
 
